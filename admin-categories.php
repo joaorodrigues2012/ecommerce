8df8c4f0a75c4edd5f0552fb504a095c6f5d9238
joaorodrigues2012 <joaorodrigues2012.jr@gmail.com>
2023-05-17 20:30:12 -0300

    CRUD - Produtos

---
+++ admin-categories.php
@@ -0,0 +1,103 @@
+<?php
+
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
+
+$app->get('/admin/categories/:idcategory/delete',function($idcategory){
+
+	User::verifyLogin();
+
+	$categories = new Category();
+
+	$categories->get((int)$idcategory);
+
+	$categories->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get('/admin/categories',function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+	$page->setTpl("categories", array(
+		"categories" => $categories
+	));
+
+});
+
+$app->get('/admin/categories/create',function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	$page->setTpl("categories-create");
+
+});
+
+$app->post('/admin/categories/create',function(){
+
+	User::verifyLogin();
+
+	$categories = new Category();
+
+	$categories->setData($_POST);
+
+	$categories->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get('/admin/categories/:idcategory',function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+	$page->setTpl("categories-update", array(
+		"category" => $category->getValues()
+	));
+
+});
+
+$app->post('/admin/categories/:idcategory',function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get('/category/:idcategory', function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", array(
+		"category"=> $category->getValues()
+	));
+
+});
