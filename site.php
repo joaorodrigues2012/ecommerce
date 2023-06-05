6b5419b4bbc10cb36403d4a92685691e6eb6dacc
joaorodrigues2012 <joaorodrigues2012.jr@gmail.com>
2023-06-04 21:40:06 -0300

    Carrinho de Compras - Calcular Frete

--- site.php
+++ site.php
@@ -64,7 +64,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", array(
 		"cart" => $cart->getValues(),
-		"products" => $cart->getProducts()
+		"products" => $cart->getProducts(),
+		"error" => Cart::getMsgError()
 	));
 });
 
@@ -111,6 +112,18 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	$cart = Cart::getFromSession();
 
 	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
 
 	header("Location: /cart");
 	exit;
