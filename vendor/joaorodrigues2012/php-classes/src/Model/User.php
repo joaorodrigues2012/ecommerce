a8f33c2fb8daa58c34c715fd618a2964dd116948
joaorodrigues2012 <joaorodrigues2012.jr@gmail.com>
2023-06-10 19:19:28 -0300

    Site - Cadastro

--- vendor/joaorodrigues2012/php-classes/src/Model/User.php
+++ vendor/joaorodrigues2012/php-classes/src/Model/User.php
@@ -261,4 +261,31 @@ class User extends Model{
     $_SESSION[User::ERROR] = NULL;
   }
 
+  public static function setErrorRegister($msg){
+    $_SESSION[User::ERROR_REGISTER] = $msg;
+  }
+
+  public static function getErrorRegister(){
+    $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : "";
+
+    User::clearErrorRegister();
+
+    return $msg;
+  }
+
+  public static function clearErrorRegister(){
+    $_SESSION[User::ERROR_REGISTER] = NULL;
+  }
+
+  public static function checkLoginExist($login){
+
+    $sql = new Sql();
+
+    $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", array(
+      ':deslogin' => $login
+    ));
+
+    return (count($results) > 0);
+  }
+
 }
